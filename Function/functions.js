22766591f229e3d129fdea58d71fefe052dfdf92
Sourabh Mane <112196535+Sourabh1028@users.noreply.github.com>
2025-01-24 13:55:40 +0000

    Arrow fun

---
+++ Function/functions.js
@@ -0,0 +1,48 @@
+
+// function printName(){
+//     console.log("SRB");
+//     console.log("SRB");
+//     console.log("SRB");
+//     console.log("SRB");console.log("SRB");
+
+//     console.log("SRB");
+// }
+
+// function add(n1, n2, ...num){
+    // if(typeof n1 == typeof n2){ 
+    //     console.log(n1 + n2);
+    // }
+    // else{ 
+    //     console.log("enter same data elements...");   
+    // }
+
+//     return num;
+// }
+// console.log(add(533,2,2,3,4));
+
+// function userLogin(username){
+//     if(!username){
+//         console.log("enter a username...");
+//         return;        
+//     }
+//     return `${username} is logged In...`
+// }
+
+// console.log(userLogin());
+
+
+// Arrow fun() -----------------------------------
+// let add = (n1, n2) => {
+//     return n1 + n2;
+// }
+// let add = (n1, n2) => (n1 + n2);
+
+// to return obj
+// let add = (n1, n2) => ({object:value});
+// console.log(add(1,2));
+
+
+// IIFE (Immediately invoked function expression)
+(function IIFE(){
+    console.log("Hello ");
+})()
