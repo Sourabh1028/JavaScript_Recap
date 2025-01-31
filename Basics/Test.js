5f88bb52a253abcc5d34ef5e7be6057c092606e4
Sourabh Mane <112196535+Sourabh1028@users.noreply.github.com>
2025-01-31 11:56:56 +0000

    loops

--- Basics/Test.js
+++ Basics/Test.js
@@ -1,10 +1,13 @@
 
-let arr = [1,2,3,4,5,6];
+// let arr = [1,2,3,4,5,6];
 
-console.log(arr);
+// console.log(arr);
 
 // console.log(arr.slice(1,3));
 
-console.log(arr.splice(1,3));
+// console.log(arr.splice(1,3));
 
-console.log(arr);
+let val = null ?? "11"; 
+
+console.log(val===11 ? "Ture" : "false");
+
---
+++ Iterations/one.js
@@ -0,0 +1,52 @@
+
+
+// for(let i=0;i<8;i++) {
+//     console.log(i);    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if(i==5){
+//         console.log("5 is Best Number");
+//     }
+//     console.log(`Value at i is ${i}`);
+// }
+
+// ------------- forof -------------
+
+// let arr=[1,2,3,4];
+// for (let i of arr) {
+//     console.log(i);    
+// }
+
+
+
+// ------------- map() -------------
+// const map = new Map();
+// map.set('IN', 'India');
+// map.set('US', 'United States');
+// map.set('ND', 'NederLand');
+
+// for (const [key, val] of map) {
+//     console.log(key, ':-', val);    
+// }
+
+const myObj = [
+    
+    {
+        language: "Javascript",
+        shortForm: "js"
+    },
+    {
+        language: "C++",
+        shortForm: "cpp"
+    }
+]
+
+arr.forEach( (i, map) => {
+    console.log(i, map);    
+})
+
+
+
+
+
